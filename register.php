0131bab5d1eade945336687683a2b67bb7ca510d
Daniel O'Connor <daniel.oconnor@gmail.com>
2012-06-13 02:43:51 +0930

    More cleanup

--- register.php
+++ register.php
@@ -104,23 +104,23 @@
 <h2>Student Information</h2>
 <table class="table-striped table-condensed">
 	<tr>
-		<th><label for="sfirst">First Name</label></th>
+		<th>First Name</th>
 		<td><input type="text" id="sfirst" name="sfirst" size="25" required="required" /></td>
 	</tr>
 
 
 	<tr>
-		<th><label for="slast">Last Name</label></th>
+		<th>Last Name</th>
 		<td><input id="slast" name="slast" type="text"  required="required" size="25"  required="required" /></td>
 	</tr>
 
 	<tr>
-		<th><label for="sschool">School</label></th>
+		<th>School</th>
 		<td><input type="text" id="sschool" name="school" size="25" /></td>
 	</tr>
 
 	<tr>
-		<th><label for="scounselor">Counselor</label></th>
+		<th>Counselor</th>
 		<td><input type="text" id="scounselor" name="scounselor" size="25" /></td>
 	</tr>
 	
@@ -133,103 +133,168 @@
 	</tr>
 
 	<tr>
-		<th><label>Graduating Class</label></th>
+		<th>Graduating Class</th>
 		<td><input id="grad" name="grad" type="text" size="4" required="required"  /></td>
 	</tr>
 </table>
 
-<div align="left">
-<b>College Lifestyle and SAT Prep In-Class Programs</b> -- (<i>includes textbook and class materials</i>)<br>
 
-<input type="radio"  name="iclass" value="ijunmar" onclick="" disabled="disabled" />June 12 - July 20 : 1:15 - 3:15 PM -- Maryknoll High Schoool -- Call Mr. Nagami at (808) 952-7340<br>
-<input type="radio"  name="iclass" value="ijulwai" onclick="addCharge()" />July 16 - July 21 : 5:00 - 7:00 PM -- Waikiki Community Center -- $190<br>
-<input type="radio"  name="iclass" value="iaugwai" onclick="addCharge()" />August 20 - August  31 : 5:00 - 7:00 PM -- Waikiki Community Center -- $190<br>
-<input type="radio"  name="iclass" value="iinfo">Please keep me informed of future In-Class Programs<p>
-<p> <br>
-
-<b>College Lifestyle and SAT Prep Interactive On-Line Webinar Programs</b><br>(<i>textbook and class materials provided by mail and email</i>)<br>
-
-<input type="radio"  name="wclass" value="wsep" onclick="addCharge()" />September 10 - September 21 : 6:30 - 8:30 PM -- $190<br>
-<input type="radio"  name="wclass" value="woct" onclick="addCharge()" />October 8 - October 19 : 6:30 - 8:30 PM -- $190<br>
-<input type="radio"  name="wclass"  value="winfo">Please keep me informed of future On-Line Programs<p>
+<h2>College Lifestyle and SAT Prep In-Class Programs</h2>
+<p><i>includes textbook and class materials</i></p>
 <p>
+<input type="radio"  name="iclass" value="ijunmar" onclick="" disabled="disabled" /> June 12 - July 20 : 1:15 - 3:15 PM -- Maryknoll High Schoool -- Call Mr. Nagami at (808) 952-7340<br>
+<input type="radio"  name="iclass" value="ijulwai" onclick="addCharge()" /> July 16 - July 21 : 5:00 - 7:00 PM -- Waikiki Community Center -- $190<br>
+<input type="radio"  name="iclass" value="iaugwai" onclick="addCharge()" /> August 20 - August  31 : 5:00 - 7:00 PM -- Waikiki Community Center -- $190<br>
+<input type="radio"  name="iclass" value="iinfo"> Please keep me informed of future In-Class Programs
+</p>
 
-</div>
-</font>
 
+<h2>College Lifestyle and SAT Prep Interactive On-Line Webinar Programs</h2>
+<p><i>textbook and class materials provided by mail and email</i></p>
+<p>
+<input type="radio"  name="wclass" value="wsep" onclick="addCharge()" /> September 10 - September 21 : 6:30 - 8:30 PM -- $190<br>
+<input type="radio"  name="wclass" value="woct" onclick="addCharge()" /> October 8 - October 19 : 6:30 - 8:30 PM -- $190<br>
+<input type="radio"  name="wclass"  value="winfo"> Please keep me informed of future On-Line Programs
+</p>
 
 <h2>Parent Information</h2>
- (All contact information is required for processing)<p>
+<p>(All contact information is required for processing)</p>
 
-First Name: <input type="text" name="pFirst" size="30">
-Last Name: <input type="text" name="pLast" size="30"><br>
-Address:<input type="text" name="pAddress" size="55"><br>
-City: <input type="text" name="pCity" size="25">   
-State:  <input type="text" name="pState" size="10">
-Zip:  <input type="text" name="pZip" size="10"><br>
-E-mail: <input type="text" name="pEmail" size="55"><br>
-Phone:  <input type="text" name="pPhone" size="15"><p>
+<table>
+	<tr>
+		<th>First Name</th>
+		<td><input type="text" name="pFirst" size="30" required="required"/></td>
+	</tr>
+	<tr>
+		<th>Last Name</th>
+		<td><input type="text" name="pLast" size="30" required="required"/></td>
+	</tr>
+	<tr>
+		<th>Address</th>
+		<td><input type="text" name="pAddress" size="55" required="required"/></td>
+	</tr>
+	<tr>
+		<th>City</th>
+		<td><input type="text" name="pCity" size="25" required="required" /></td>
+	</tr>
+	<tr>
+		<th>State</th>
+		<td><input type="text" name="pState" size="10" required="required" /></td>
+	</tr>
 
-Military?  
-<input type="radio" id="militaryyes" name="military" value="yes" onclick="addCharge()" />Yes 
-			<input type="radio" id="militaryno" name="military" value="no" checked="checked" />No   (If Yes, please answer the following)<br>
-Installation: 
-			<input type="text" name="installation" size="28" />   Job Title 
-			<input type="text" name="jobTitle" size="28" />   Rank <input type="text" name="rank" size="28" />
+	<tr>
+		<th>Zip</th>
+		<td><input type="text" name="pZip" size="10" required="required"/></td>
+	</tr>
+
+	<tr>
+		<th>Email</th>
+		<td><input type="email" name="pEmail" size="55" required="required"/></td>
+	</tr>
+
+	<tr>
+		<th>Phone</th>
+		<td><input type="tel" name="pPhone" size="15" required="required"/></td>
+	</tr>
+
+
+	<tr>
+		<th>Military?</th>
+		<td>
+			<input type="radio" id="militaryyes" name="military" value="yes" onclick="addCharge()" />Yes 
+			<input type="radio" id="militaryno" name="military" value="no" checked="checked" />No
+			<hr />
+			If Yes, please answer the following
+		</td>
+	</tr>
+	<tr>
+		<th>Installation</th>
+		<td><input type="text" name="installation" size="28" /></td>
+	</tr>
+	<tr>
+		<th>Job Title</th>
+		<td><input type="text" name="jobTitle" size="28" /></td>
+	</tr>
+	<tr>
+		<th>Rank</th>
+		<td><input type="text" name="rank" size="28" /></td>
+	</tr>
+	<tr>
+		<th>IMCEA?</th>
+		<td><input type="radio" id="imceayes" name="imcea" value="yes" onclick="addCharge()" />Yes 
+				<input type="radio" id="imceano" name="imcea" value="no" checked="checked">No</td>
+	</tr>
+	<tr>
+		<th>IMCEA Member Number</th>
+		<td><input type="text" name="memberNumber" size="10" /></td>
+	</tr>
+</table>
+
+<h2>Payment	Information</h2>
 <p>
-IMCEA?  
-<input type="radio" id="imceayes" name="imcea" value="yes" onclick="addCharge()" />Yes 
-			<input type="radio" id="imceano" name="imcea" value="no" checked="checked">No   IMCEA Member Number: 
-			<input type="text" name="memberNumber" size="10" /><p>
-			
-		<table><tr><td>	
-Payment	Information:  </font></b>  <img border="0" src="visa.gif" width="44" height="28" hspace="3">     
-			<img border="0" src="mastercard.gif" width="44" height="28" hspace="3">     
-			<img border="0" src="amex.gif" width="44" height="28" hspace="3">   
-			<img border="0" src="discover.gif" width="44" height="28" hspace="3"></td>
-		</tr>
- 
- 
-		<tr>
-		<td colspan="4"><font face="arial" size="4" >Credit Card Type:  
-		<input type="hidden" name="paybycredit" value="1">	
+	<img border="0" src="visa.gif" width="44" height="28" hspace="3">
+	<img border="0" src="mastercard.gif" width="44" height="28" hspace="3">     
+	<img border="0" src="amex.gif" width="44" height="28" hspace="3">   
+	<img border="0" src="discover.gif" width="44" height="28" hspace="3">
+</p>
+<p><small>If paying by check please print this
+		 form and mail with payment to <address>Every Teen Can Succeed, 2509 Ala Wai Blvd. #801-A, Honolulu, HI 96815</address></small></p>
+<table>
+	<tr>
+		<th>Credit Card Type</th>
+		<td>
+			<input type="hidden" name="paybycredit" value="1" />	
 			<select size="1" name="payment_type">
-			<option value="Visa">Visa</option>
-			<option value="MasterCard">MasterCard</option>
-			<option value="American Express">American Express</option>
-			<option value="Discover">Discover</option>
-			<option value="Check">Check</option>
-			</select><br>
-<small>If paying by check please print this
-		 form and mail with payment to <b>Every Teen Can Succeed, 2509 Ala Wai Blvd. #801-A, Honolulu, HI 96815</b></small><br> <p></font><font size="4" face="arial">
-		 Name on Card<span style="color: #f00">*</span>: 
-			<input type="text" name="UMname" size="30" />   
-			Card Billing Zipcode<span style="color: #f00">*</span>: 
+				<option value="Visa">Visa</option>
+				<option value="MasterCard">MasterCard</option>
+				<option value="American Express">American Express</option>
+				<option value="Discover">Discover</option>
+				<option value="Check">Check</option>
+			</select>
+		</td>
+
+	</tr>
+	<tr>
+		<th>Name on Card</th>
+		<td><input type="text" name="UMname" size="30" required="required" />   </td>
+	</tr>
+
+	<tr>
+		<th>Card Billing Zipcode</th> 
+		<td><input type="text" name="UMzip" size="15" required="required" /></td>
+	</tr>
+	<tr>
+		<th>Email Address</th>
+		<td><input type="text" name="UMcustemail" size="50" required="required" />
+		 <small>(to send receipt)</small></td>
+	</tr>
+	<tr>
+		<th>Card Number</th>
+		<td><input type="text" name="UMcard" size="17" required="required"  /> </td>   
+		</tr>
+	<tr>
+		<th>Card Expiration Date (MMYY)</th>
+		<td><input type="text" name="UMexpir" size="4"  required="required"  /> <small>(no space or dash please)</small></td>
+	</tr>
 			
-			<input type="text" name="UMzip" size="15" /><p>
-			Email Address<span style="color: #f00">*</span> <small><font color="000080">(to send receipt)</small>: 
-			</font><p>
-			 
-			<input type="text" name="UMcustemail" size="50" /><p>
-			Card Number<span style="color: #f00">*</span>: <input type="text" name="UMcard" size="17" />    
-			   Card Expiration Date<span style="color: #f00">*</span> <small><font color="#000080"> (MMYY)</font></small>:  
-			<input type="text" name="UMexpir" size="4" /><br><small><font color="000080">(no space or dash please)</small></font><p>
-			 
-			Card ID (CVV2/CID) Number<span style="color: #f00">*</span>:<br>
-            (<a target="_blank" href="/cvv.htm"><small>What is the Card ID?</small></a>) 
-			<input type="text" name="UMcvv2" size="5" />
-		    
-			<div align="right"><b><i>Total Amount to Charge</i></b><small><font color="000080"> ($USD)</small></font>: $ <input type="text" id="amount_box" name="UMamount" value="N/A" /></div><p>
-<center>Call <i><b>Every Teen Can Succeed</b></i> at <b>(808) 372-3626</b> with any payment questions</center><p></font>
-</div>	<p align="center">
-							<input type="submit" id="submit_button" name="submit_button" value="Register Now" onclick="return verify();" /><br>
+	<tr>
+		<th>Card ID (CVV2/CID) Number</th>
+		<td><input type="text" name="UMcvv2" size="5" /> <a target="_blank" href="/cvv.htm"><small>What is the Card ID?</small></a></td>
+	</tr>
+</table>
+<div align="right">
+	<b><i>Total Amount to Charge</i></b>
+	<small>($USD)</small>: $ <input type="text" id="amount_box" name="UMamount" value="N/A" /></div>
+	
+	<p><center>Call <i><b>Every Teen Can Succeed</b></i> at <b>(808) 372-3626</b> with any payment questions</center></p>
+</div>
+<p align="center"><input type="submit" id="submit_button" name="submit_button" value="Register Now" onclick="return verify();" />
 							
-</td></tr><tr><td align="right">
+<!--
 <div align="right"><span id="siteseal"><script type="text/javascript" src="https://seal.starfieldtech.com/getSeal?sealID=TBUvlHEvPDG4JpKHHOGg9eSESBcvtVMEYDGLi8hIcneIHCU62QH4etkyZq0"></script></span>
 <script type="text/JavaScript">var TFN='';var TFA='';var TFI='0';var TFL='0';var tf_RetServer="rt.trafficfacts.com";var tf_SiteId="fed17602acba26e1fd9083e89872beaa75503e5a";var tf_ScrServer=document.location.protocol+"//rt.trafficfacts.com/tf.php?k=fed17602acba26e1fd9083e89872beaa75503e5a;c=s;v=5";document.write(unescape('%3Cscript type="text/JavaScript" src="'+tf_ScrServer+'">%3C/script>'));</script><noscript><img src="http://rt.trafficfacts.com/ns.php?k=fed17602acba26e1fd9083e89872beaa75503e5a" height="1" width="1" alt=""/></noscript>
-</div><p align="right">
-			</td>
-</td></tr></table>
+</div>
+-->
 </form>
 </div>
 </body>
